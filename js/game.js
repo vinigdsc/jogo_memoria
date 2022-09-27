6cf6ec901627c73e7176c6485aa49b18ad3650b4
Vinicius Caetano <viniciusgcae@gmail.com>
2022-09-27 14:21:55 -0300

    finalização do game

--- js/game.js
+++ js/game.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector(".grid");
+const spanPlayer = document.querySelector(".player");
+const timer = document.querySelector(".timer");
 
 const characters = [
   "beth",
@@ -25,8 +27,11 @@ let secondCard = "";
 const checkEndGame = () => {
   const disabledCards = document.querySelectorAll(".disabled-card");
 
-  if (disabledCards.lenght === 20) {
-    alert("Parabéns, você conseguiu!");
+  if (disabledCards.length === 20) {
+    clearInterval(this.loop);
+    alert(
+      `Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi: ${timer.innerHTML}`
+    );
   }
 };
 
@@ -95,4 +100,15 @@ const loadGame = () => {
   });
 };
 
-loadGame();
+const startTimer = () => {
+  this.loop = setInterval(() => {
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1;
+  }, 1000);
+};
+
+window.onload = () => {
+  spanPlayer.innerHTML = localStorage.getItem("player");
+  startTimer();
+  loadGame();
+};
